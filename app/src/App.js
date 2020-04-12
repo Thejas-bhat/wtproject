89f5d2fe636d58017c4ac93748225267761d31ba
Thejas-bhat <thejasbhat.6@gmail.com>
2020-04-12 19:55:35 +0530

    displaying the server sent image works

--- app/src/App.js
+++ app/src/App.js
@@ -107,11 +107,23 @@ class Upload extends Component {
     data.append('file', file);
     data.append('filename', "gen.png");
 
+    var myImage = document.querySelector('#res');
     fetch('http://0.0.0.0:5000/upload', {
-      method: 'POST',
-      body: data,
+          method: 'POST',
+          body: data,
+        }).then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.blob();
+    })
+    .then((myBlob) => {
+      myImage.src = URL.createObjectURL(myBlob);
+      
+    })
+    .catch((error) => {
+      console.error('There has been a problem with your fetch operation:', error);
     });
-
 
     reader.onloadend = () => {
       this.setState({
