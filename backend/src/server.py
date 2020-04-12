f0380280dcfac06389e9592fe05d095caf46aa4d
tejvi-m <tejvimstays@gmail.com>
2020-04-12 18:58:48 +0530

    resolved CORS issues

--- backend/src/server.py
+++ backend/src/server.py
@@ -1,7 +1,8 @@
 import os
 from flask import Flask, flash, request, redirect, url_for, session, send_file
 from werkzeug.utils import secure_filename
-# from flask_cors import CORS, cross_origin
+from flask_cors import CORS, cross_origin
+import flask_cors
 import logging
 
 logging.basicConfig(level=logging.INFO)
@@ -15,6 +16,7 @@ ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+flask_cors.CORS(app, expose_headers="Access-Control-Allow-Origin: *")
 
 @app.route('/upload', methods=['POST'])
 def fileUpload():
@@ -30,10 +32,8 @@ def fileUpload():
     response="Whatever you wish to return"
 
 
-    return send_file("./dummy.png", mimetype='image/gif')
+    return send_file("/home/tejvi/WTProject/test_docs/code2.png", mimetype='image/gif')
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(24)
     app.run(debug=True,host="0.0.0.0", port=5000, use_reloader=False)
-
-# flask_cors.CORS(app, expose_headers='Authorization')
